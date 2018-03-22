3f7415f36898d2edf97734848e9cf376b6647b98
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2018-03-22 14:16:59 +0300

    No M-failed-other for staged PRs which are restarted or closed (#8)
    If somebody aborted PR staging (e.g., requesting changes),
    we just need to restart, clearing staging labels (no error here).
    Similarly, there is nothing unusual if somebody manually merged
    a staged PR (the bot can be configured with guarded_run).
    The bot should not complain and just clean labels up in this case.

--- src/MergeContext.js
+++ src/MergeContext.js
@@ -49,7 +49,7 @@ class MergeContext {
 
         if (!this._prOpen()) {
             this._log("was unexpectedly closed");
-            return await this._cleanupMergeFailed(true);
+            return await this._cleanupMergeFailed(true, this._labelCleanStaged);
         }
 
         const commitStatus = await this._checkStatuses(this._tagSha, Config.stagingChecks());
@@ -72,11 +72,11 @@ class MergeContext {
         // but we check compareStatus first to avoid useless api requests
         if (compareStatus === "diverged") {
             this._log("PR branch and it's base branch diverged");
-            return await this._cleanupMergeFailed(true);
+            return await this._cleanupMergeFailed(true, this._labelCleanStaged);
         }
         if (await this._needRestart()) {
             this._log("PR will be restarted");
-            return await this._cleanupMergeFailed(true);
+            return await this._cleanupMergeFailed(true, this._labelCleanStaged);
         }
 
         assert(compareStatus === "ahead");
@@ -490,6 +490,13 @@ class MergeContext {
                 Config.passedStagingChecksLabel()
                 ]);
         await this._addLabel(Config.failedOtherLabel());
+    }
+
+    async _labelCleanStaged() {
+        await this._removeLabelsIf([
+                Config.waitingStagingChecksLabel(),
+                Config.passedStagingChecksLabel()
+        ]);
     }
 
     async _labelFailedStagingChecks() {
