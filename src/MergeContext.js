78b15f475c7ba0d9eee958593010e87f30cc4a6a
Eduard Bagdasaryan <dictiano@gmail.com>
2018-05-17 09:50:02 +0300

    Revert "Restart merging if the PR message was modified"
    This reverts commit 04cf2492490adfc5675c57e7c3bce8d138bb2121.

--- src/MergeContext.js
+++ src/MergeContext.js
@@ -311,16 +311,7 @@ class MergeContext {
         const tagCommit = await GH.getCommit(this._tagSha);
         const prMergeSha = await GH.getReference(this._mergePath());
         const prCommit = await GH.getCommit(prMergeSha);
-        if (tagCommit.tree.sha !== prCommit.tree.sha) {
-            this._log("tag freshness: no (sha mismatch)");
-            return false;
-        }
-        if (this._prMessage() !== tagCommit.message) {
-            this._log("tag freshness: no (message mismatch)");
-            return false;
-        }
-        this._log("tag freshness: yes");
-        return true;
+        return tagCommit.tree.sha === prCommit.tree.sha;
     }
 
     // whether the staged commit and the base HEAD have independent,
