0a08cd7f59f888159d7ffbed069355b9051a97ec
Eduard Bagdasaryan <dictiano@gmail.com>
2019-04-03 18:21:45 +0300

    Prohibit non-ASCII symbols in commit messages

--- src/MergeContext.js
+++ src/MergeContext.js
@@ -787,9 +787,23 @@ class PullRequest {
         return (this._rawPr.title + ' (#' + this._rawPr.number + ')' + '\n\n' + this._prBody()).trim();
     }
 
+    _invalidCharacterPosition(str) {
+        const prohibitedCharacters = /[^\u0020-\u007f]+/; // allow non-special ASCII characters
+        const match = prohibitedCharacters.exec(str);
+        if (match)
+            return match.index;
+        return -1;
+    }
+
     _prMessageValid() {
         const lines = this._prMessage().split('\n');
-        for (let line of lines) {
+        for (let i = 0; i < lines.length; ++i) {
+            const line = lines[i];
+            const invalidPosition = this._invalidCharacterPosition(line);
+            if (invalidPosition !== -1) {
+                this._warn("PR message has an invalid character at (" + i + "," + invalidPosition + ")");
+                return false;
+            }
             if (line.length > 72)
                 return false;
         }
