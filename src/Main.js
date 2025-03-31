7c2eac206770b051df1d466414c500a8f3c2b253
Eduard Bagdasaryan <dictiano@gmail.com>
2025-03-31 15:29:52 +0300

    Simplified HandlerWrap callers

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const createHandler = require('github-webhook-handler');
 const Config = require('./Config.js');
 const Log = require('./Logger.js');
@@ -19,50 +20,48 @@ WebhookHandler.on('error', (err) => {
    Logger.error('Error:', err.message);
 });
 
-function HandlerWrap(ev, handler) {
-    let prIds = null;
-    try {
-        prIds = handler(ev);
-        assert(prIds);
-        if (prIds.length === 0)
-            return;
-    } catch (err) {
-        Logger.error("Error in", err.message);
-    }
-    Merger.run(prIds);
+function HandlerWrap(handler) {
+    return (ev) => {
+        let prIds = null;
+        try {
+            prIds = handler(ev);
+            assert(prIds);
+            if (prIds.length === 0)
+                return;
+        } catch (err) {
+            Logger.error("Error in", err.message);
+        }
+        Merger.run(prIds);
+    };
 }
 
 // https://developer.github.com/v3/activity/events/types/#pullrequestreviewevent
-WebhookHandler.on('pull_request_review', (anEv) => {
-    HandlerWrap(anEv, (ev) => {
+WebhookHandler.on('pull_request_review', HandlerWrap((ev) => {
         const pr = ev.payload.pull_request;
         Logger.info("pull_request_review event:", ev.payload.id, pr.number, pr.head.sha, pr.state);
         return Util.PrId.PrNum(pr.number);
-    });
-});
+    })
+);
 
 // https://developer.github.com/v3/activity/events/types/#pullrequestevent
-WebhookHandler.on('pull_request', (anEv) => {
-    HandlerWrap(anEv, (ev) => {
+WebhookHandler.on('pull_request', HandlerWrap((ev) => {
         const pr = ev.payload.pull_request;
         Logger.info("pull_request event:", ev.payload.id, pr.number, pr.head.sha, pr.state);
         return Util.PrId.PrNum(pr.number);
-    });
-});
+    })
+);
 
 // https://developer.github.com/v3/activity/events/types/#statusevent
-WebhookHandler.on('status', (anEv) => {
-    HandlerWrap(anEv, (ev) => {
+WebhookHandler.on('status', HandlerWrap((ev) => {
         const e = ev.payload;
         Logger.info("status event:", e.id, e.sha, e.context, e.state);
         const branches = Array.from(e.branches, b => b.name);
         return Util.PrId.BranchList(branches, e.commit.commit.message);
-    });
-});
+    })
+);
 
 // https://developer.github.com/v3/activity/events/types/#pushevent
-WebhookHandler.on('push', (anEv) => {
-    HandlerWrap(anEv, (ev) => {
+WebhookHandler.on('push', HandlerWrap((ev) => {
         const e = ev.payload;
         Logger.info("push event:", e.ref);
 
@@ -70,12 +69,11 @@ WebhookHandler.on('push', (anEv) => {
         const parts = e.ref.split('/');
         const branch = parts[parts.length-1];
         return Util.PrId.BranchList([branch], e.head_commit ? e.head_commit.message : null);
-    });
-});
+    })
+);
 
 // https://docs.github.com/ru/webhooks/webhook-events-and-payloads#workflow_run
-WebhookHandler.on('workflow_run', (anEv) => {
-    HandlerWrap(anEv, (ev) => {
+WebhookHandler.on('workflow_run', HandlerWrap((ev) => {
         const e = ev.payload.workflow_run;
         Logger.info("workflow_run event:", e.head_sha);
         // e.pull_requests is empty for the staged commit
@@ -89,12 +87,11 @@ WebhookHandler.on('workflow_run', (anEv) => {
         }
         const list = Array.from(e.pull_requests, v => v.number);
         return Util.PrId.PrNumList(list);
-    });
-});
+    })
+);
 
 // https://docs.github.com/en/webhooks/webhook-events-and-payloads#check_run
-WebhookHandler.on('check_run', (anEv) => {
-    HandlerWrap(anEv, (ev) => {
+WebhookHandler.on('check_run', HandlerWrap((ev) => {
         const e = ev.payload.check_run;
         Logger.info("check_run event:", e.head_sha);
         // e.check_suite.pull_requests is empty for the staged commit
@@ -108,8 +105,8 @@ WebhookHandler.on('check_run', (anEv) => {
         }
         const list = Array.from(e.check_suite.pull_requests, v => v.number);
         return Util.PrId.PrNumList(list);
-    });
-});
+    })
+);
 
 WebhookHandler.on('ping', (ev) => {
     const e = ev.payload;
