measurement-factory/anubis

commit c1fac18c530d4a8a3380635746e594eaacd803f9

Upgrade Octokit library to v21.1.0; use ES6 import declarations (#73)
This fixes the Octokit bug affecting some getStatuses() calls:

     Unexpected end of JSON input #649

Also switched from Node require() calls to ES6 import declarations
because Octokit ships as a pure ECMAScript Module (ESM) since v21.0.0,
and require() does not support ESM loading. Sorted import declarations.

XXX: After the declarations update, eslint reports an error:

     'import' and 'export' may appear only with 'sourceType: module'

Fixing that requires noisy changes that deserve a dedicated commit.

src/GitHubUtil.js
@@ -1,6 +1,10 @@
-const assert = require('assert');
-const Config = require('./Config.js');
-const { Octokit }  = require("@octokit/rest");
+import * as Log from './Logger.js';
+import * as Util from './Util.js';
+import Config from './Config.js';
+
+import assert from 'assert';
+import { Octokit } from "@octokit/rest";
+
 const GitHub = new Octokit({
     auth: Config.githubToken(),
     request: {
@@ -10,9 +14,6 @@ const GitHub = new Octokit({
     },
     baseUrl: Config.baseUrl()
 });
-const Util = require('./Util.js');
-const Log = require('./Logger.js');
-
 const ErrorContext = Util.ErrorContext;
 const commonParams = Util.commonParams;
 const logApiResult = Log.logApiResult;
@@ -61,7 +62,7 @@ async function paginatedGet(githubMethod, params) {
     return result;
 }
 
-async function getOpenPrs() {
+export async function getOpenPrs() {
     let params = commonParams();
 
     let data = await paginatedGet(GitHub.rest.pulls.list, params);
@@ -71,7 +72,7 @@ async function getOpenPrs() {
     return data;
 }
 
-async function getLabels(prNum) {
+export async function getLabels(prNum) {
     let params = commonParams();
     params.issue_number = prNum;
 
@@ -83,7 +84,7 @@ async function getLabels(prNum) {
 // Gets PR metadata from GitHub
 // If requested and needed, retries until GitHub calculates PR mergeable flag.
 // Those retries, if any, are limited to a few minutes.
-async function getPR(prNum, awaitMergeable) {
+export async function getPR(prNum, awaitMergeable) {
     const max = 64 * 1000 + 1; // ~2 min. overall
     for (let d = 1000; d < max; d *= 2) {
         const pr = await getRawPR(prNum);
@@ -107,7 +108,7 @@ async function getRawPR(prNum) {
     return await rateLimitedPromise(result);
 }
 
-async function getReviews(prNum) {
+export async function getReviews(prNum) {
     let params = commonParams();
     params.pull_number = prNum;
 
@@ -116,7 +117,7 @@ async function getReviews(prNum) {
     return reviews;
 }
 
-async function getCheckRuns(ref) {
+export async function getCheckRuns(ref) {
     let params = commonParams();
     params.ref = ref;
 
@@ -125,7 +126,7 @@ async function getCheckRuns(ref) {
     return checkRuns;
 }
 
-async function getStatuses(ref) {
+export async function getStatuses(ref) {
     let params = commonParams();
     params.ref = ref;
 
@@ -134,7 +135,7 @@ async function getStatuses(ref) {
     return await rateLimitedPromise(result);
 }
 
-async function getCommit(sha) {
+export async function getCommit(sha) {
     let params = commonParams();
     params.commit_sha = sha;
 
@@ -143,7 +144,7 @@ async function getCommit(sha) {
     return await rateLimitedPromise(result);
 }
 
-async function createCommit(treeSha, message, parents, author, committer) {
+export async function createCommit(treeSha, message, parents, author, committer) {
     assert(!Config.dryRun());
     let params = commonParams();
     params.tree = treeSha;
@@ -158,7 +159,7 @@ async function createCommit(treeSha, message, parents, author, committer) {
 }
 
 // returns one of: "ahead", "behind", "identical" or "diverged"
-async function compareCommits(baseRef, headRef) {
+export async function compareCommits(baseRef, headRef) {
     let params = commonParams();
     params.basehead = `${baseRef}...${headRef}`;
 
@@ -167,7 +168,7 @@ async function compareCommits(baseRef, headRef) {
     return (await rateLimitedPromise(result)).status;
 }
 
-async function getCommits(branch, since) {
+export async function getCommits(branch, since) {
     let params = commonParams();
     params.sha = branch; // sha or branch to start listing commits from
     params.since = since;
@@ -177,7 +178,7 @@ async function getCommits(branch, since) {
     return commits;
 }
 
-async function getReference(ref) {
+export async function getReference(ref) {
     let params = commonParams();
     params.ref = ref;
 
@@ -186,7 +187,7 @@ async function getReference(ref) {
     return (await rateLimitedPromise(result)).object.sha;
 }
 
-async function updateReference(ref, sha, force) {
+export async function updateReference(ref, sha, force) {
     assert(!Config.dryRun());
     assert((ref === Config.stagingBranchPath()) || !Config.stagedRun());
 
@@ -201,7 +202,7 @@ async function updateReference(ref, sha, force) {
 }
 
 
-async function updatePR(prNum, state) {
+export async function updatePR(prNum, state) {
     assert(!Config.dryRun());
 
     let params = commonParams();
@@ -213,7 +214,7 @@ async function updatePR(prNum, state) {
     return await rateLimitedPromise(result);
 }
 
-async function addLabels(params) {
+export async function addLabels(params) {
     assert(!Config.dryRun());
 
     const result = await GitHub.rest.issues.addLabels(params);
@@ -221,7 +222,7 @@ async function addLabels(params) {
     return await rateLimitedPromise(result);
 }
 
-async function removeLabel(label, prNum) {
+export async function removeLabel(label, prNum) {
     assert(!Config.dryRun());
 
     let params = commonParams();
@@ -253,7 +254,7 @@ async function removeLabel(label, prNum) {
 //    });
 //}
 
-async function createStatus(sha, state, targetUrl, description, context) {
+export async function createStatus(sha, state, targetUrl, description, context) {
     assert(!Config.dryRun());
 
     let params = commonParams();
@@ -268,7 +269,7 @@ async function createStatus(sha, state, targetUrl, description, context) {
     return (await rateLimitedPromise(result)).context;
 }
 
-async function getProtectedBranchRequiredStatusChecks(branch) {
+export async function getProtectedBranchRequiredStatusChecks(branch) {
     let params = commonParams();
     params.branch = branch;
 
@@ -277,7 +278,7 @@ async function getProtectedBranchRequiredStatusChecks(branch) {
     return (await rateLimitedPromise(result)).protection.required_status_checks.contexts;
 }
 
-async function getUser(username) {
+export async function getUser(username) {
     const params = commonParams();
     params.username = username;
 
@@ -286,33 +287,11 @@ async function getUser(username) {
     return await rateLimitedPromise(result);
 }
 
-async function getUserEmails() {
+export async function getUserEmails() {
     const params = commonParams();
 
     const result = await GitHub.rest.users.listEmailsForAuthenticatedUser(params);
     logApiResult(getUserEmails.name, params, {emails: result.data});
     return await rateLimitedPromise(result);
 }
-
-module.exports = {
-    getOpenPrs: getOpenPrs,
-    getLabels: getLabels,
-    getPR: getPR,
-    getReviews: getReviews,
-    getStatuses: getStatuses,
-    getCheckRuns: getCheckRuns,
-    getCommit: getCommit,
-    getCommits: getCommits,
-    createCommit: createCommit,
-    compareCommits: compareCommits,
-    getReference: getReference,
-    updateReference: updateReference,
-    updatePR: updatePR,
-    addLabels: addLabels,
-    removeLabel: removeLabel,
-    createStatus: createStatus,
-    getProtectedBranchRequiredStatusChecks: getProtectedBranchRequiredStatusChecks,
-    getUser: getUser,
-    getUserEmails: getUserEmails
-};
 

src/MergeContext.js
@@ -1,9 +1,9 @@
-const assert = require('assert');
-const Config = require('./Config.js');
-const Log = require('./Logger.js');
-const GH = require('./GitHubUtil.js');
-const Util = require('./Util.js');
+import * as GH from './GitHubUtil.js';
+import * as Log from './Logger.js';
+import * as Util from './Util.js';
+import Config from './Config.js';
 
+import assert from 'assert';
 
 // Process() outcome
 class ProcessResult
@@ -1795,13 +1795,8 @@ class PullRequest {
 }
 
 // promises to update/advance the given PR, hiding PullRequest from callers
-function Process(rawPr, banStaging) {
+export function Process(rawPr, banStaging) {
     let pr = new PullRequest(rawPr, banStaging);
     return pr.process();
 }
-
-
-module.exports = {
-    Process: Process
-};
 

src/Util.js
@@ -1,12 +1,13 @@
-const assert = require('assert');
-const Config = require('./Config.js');
+import Config from './Config.js';
 
-function sleep(msec) {
+import assert from 'assert';
+
+export function sleep(msec) {
     return new Promise((resolve) => setTimeout(resolve, msec));
 }
 
 // common parameters for all API calls
-function commonParams() {
+export function commonParams() {
     return {
         owner: Config.owner(),
         repo: Config.repo()
@@ -15,7 +16,7 @@ function commonParams() {
 
 const PrNumberRegex = / \(#(\d+)\)$/;
 
-function ParsePrNumber(prMessage) {
+export function ParsePrNumber(prMessage) {
     assert(prMessage);
     const lines = prMessage.split(/\r*\n/);
     const matched = lines[0].match(PrNumberRegex);
@@ -28,7 +29,7 @@ function ParsePrNumber(prMessage) {
 }
 
 // An error context for promisificated wrappers.
-class ErrorContext extends Error {
+export class ErrorContext extends Error {
     // The underlying rejection may be a bot-specific Promise.reject() or
     // be caused by a GitHub API error, so 'err' contains either
     // an error string or the entire API error object.
@@ -69,7 +70,7 @@ class ErrorContext extends Error {
 // PR number, or
 // PR branch name (without 'refs' or 'heads' prefixes), or
 // staging branch commit SHA (including stale commits).
-class PrId
+export class PrId
 {
     constructor(type, val, msg) {
         assert(type !== undefined);
@@ -88,12 +89,4 @@ class PrId
 
     toString() { return `${this.type}:${this.value}`; }
 }
-
-module.exports = {
-    sleep: sleep,
-    commonParams: commonParams,
-    ParsePrNumber: ParsePrNumber,
-    ErrorContext: ErrorContext,
-    PrId: PrId,
-};
 

src/Config.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const timestring = require('timestring');
-const assert = require('assert');
+import assert from 'assert';
+import fs from 'fs';
+import timestring from 'timestring';
 
 class ConfigOptions {
     constructor(fname) {
@@ -142,4 +142,4 @@ class ConfigOptions {
 const configFile = process.argv.length > 2 ? process.argv[2] : './config.json';
 const Config = new ConfigOptions(configFile);
 
-module.exports = Config;
+export default Config;

src/Logger.js
@@ -1,20 +1,21 @@
-const assert = require('assert');
-const bunyan = require('bunyan');
-const Config = require('./Config.js');
+import Config from './Config.js';
 
-const Logger = bunyan.createLogger(Config.loggerParams());
+import assert from 'assert';
+import bunyan from 'bunyan';
 
-function LogError(err, context) {
+export const Logger = bunyan.createLogger(Config.loggerParams());
+
+export function LogError(err, context) {
     Log(err, context, "error");
 }
 
-function LogException(err, context) {
+export function LogException(err, context) {
     Log(err, context, "info");
 }
 
-function Log(err, context, kind) {
+export function Log(err, context, kind) {
     assert(context);
-    msg = context + ": ";
+    let msg = context + ": ";
     // non-Error exceptions, like strings
     if (err.message === undefined) {
         msg += JSON.stringify(err);
@@ -31,14 +32,7 @@ function Log(err, context, kind) {
         Logger.info(msg);
 }
 
-function logApiResult(method, params, result) {
+export function logApiResult(method, params, result) {
     Logger.info(method, "OK, params:", JSON.stringify(params), "result:", JSON.stringify(result));
 }
-
-module.exports = {
-    Logger: Logger,
-    LogError: LogError,
-    LogException: LogException,
-    logApiResult: logApiResult
-};
 

src/Main.js
@@ -1,12 +1,13 @@
-const assert = require('assert');
-const createHandler = require('github-webhook-handler');
-const Config = require('./Config.js');
-const Log = require('./Logger.js');
-const Merger = require('./RepoMerger.js');
-const Util = require('./Util.js');
+import * as Log from './Logger.js';
+import * as RepoMerger from './RepoMerger.js';
+import * as Util from './Util.js';
+import Config from './Config.js';
+
+import assert from 'assert';
+import createHandler from 'github-webhook-handler';
 
 const Logger = Log.Logger;
-
+const Merger = RepoMerger.Merger;
 const WebhookHandler = createHandler({ path: Config.githubWebhookPath(), secret: Config.githubWebhookSecret() });
 
 process.on('unhandledRejection', error => {

src/PrMerger.js
@@ -1,10 +1,12 @@
-const assert = require('assert');
-const Config = require('./Config.js');
-const Log = require('./Logger.js');
+import * as GH from './GitHubUtil.js';
+import * as Log from './Logger.js';
+import * as MergeContext from './MergeContext.js';
+import * as Util from './Util.js';
+import Config from './Config.js';
+
+import assert from 'assert';
+
 const Logger = Log.Logger;
-const GH = require('./GitHubUtil.js');
-const Util = require('./Util.js');
-const MergeContext = require('./MergeContext.js');
 
 class PrScanResult {
     constructor(prs) {
@@ -278,7 +280,7 @@ class PrMerger {
 } // PrMerger
 
 // promises to process all PRs once, hiding PrMerger from callers
-async function Step(prIds) {
+export async function Step(prIds) {
     assert(prIds !== undefined);
     if (prIds !== null)
         Logger.info('prIds: [' + prIds.join() + ']');
@@ -288,6 +290,4 @@ async function Step(prIds) {
     _LastScan = await mergerer.execute(lastScan, prIds);
     return _LastScan.minDelay;
 }
-
-module.exports = Step;
 

src/RepoMerger.js
@@ -1,9 +1,10 @@
-const assert = require('assert');
-const http = require('http');
-const Config = require('./Config.js');
-const Log = require('./Logger.js');
-const Util = require('./Util.js');
-const Step = require('./PrMerger.js');
+import * as Log from './Logger.js';
+import * as PrMerger from './PrMerger.js';
+import * as Util from './Util.js';
+import Config from './Config.js';
+
+import assert from 'assert';
+import http from 'http';
 
 const Logger = Log.Logger;
 
@@ -41,7 +42,7 @@ class RepoMerger {
                 params.host = Config.host();
             this._server.listen(params, () => {
                 let hostStr = Config.host() ? Config.host() : "unspecified";
-                Log.Logger.info("HTTP server started and listening on " + hostStr + ":" + Config.port());
+                Logger.info("HTTP server started and listening on " + hostStr + ":" + Config.port());
                 resolve(true);
             });
         });
@@ -74,7 +75,7 @@ class RepoMerger {
                 this._prIds = [];
                 if (!this._server)
                     await this._createServer();
-                rerunIn = await Step(ids);
+                rerunIn = await PrMerger.Step(ids);
             } catch (e) {
                 Log.LogError(e, "RepoMerger.run");
                 this._rerun = true;
@@ -133,7 +134,5 @@ class RepoMerger {
     }
 }
 
-const Merger = new RepoMerger();
-
-module.exports = Merger;
+export const Merger = new RepoMerger();
 

commit 8fba05e285b8b1771fb85c508c2df50ccac94beb

Wait for status checks to be copied before attempting to merge (#79)
After we apply required status checks to a staged commit and
subsequently attempt to merge that commit, GitHub may return an error
that the staged commit lacks those required checks. Evidently, GitHub
responds to a createStatus() call before that call has an effect, and it
may take a few seconds for the checks to be actually applied. Now we
wait up to 2 minutes and report an error if the status is still missing.

Also removed ErrorContext class and fixed the related exception
handlers. ErrorContext was used in old Anubis versions in promisificated
wrappers of Octokit API calls. After updating Octokit to v21 at c1fac18,
ErrorContext was eliminated from all wrappers but one getPR() call.
Now the last use case is gone and hence, the class itself too.

Also fixed broken exception handlers with ErrorContext checks
(unreachable since c1fac18). We should analyze these 404 and 422 errors
using Octokit-generated RequestError instead. The new error handling
differs from the old one: Previously, we treated 404 and 422 errors, as
well as all non-HTTP errors, as 'notFound()' or 'unprocessale()',
depending on the context.  Now we consider only genuine 404 and 422
errors as such.  However, this behavior change should probably not
matter now since the old behavior has been broken since c1fac18.

src/GitHubUtil.js
@@ -14,7 +14,6 @@ const GitHub = new Octokit({
     },
     baseUrl: Config.baseUrl()
 });
-const ErrorContext = Util.ErrorContext;
 const commonParams = Util.commonParams;
 const logApiResult = Log.logApiResult;
 
@@ -81,21 +80,38 @@ export async function getLabels(prNum) {
     return await rateLimitedPromise(result);
 }
 
+/// For waitFor() code to signal that more iterations are needed
+const TryAgain = Symbol("TryAgain");
+
+// (Re)runs `code` while it returns a TryAgain value, waiting between retries.
+// Meant for `code` that makes GitHub API call(s) that may not succeed on the
+// first try.
+async function waitFor(description, code) {
+    // this limits total wait time to about 2 minutes
+    const singleWaitMax = 64; // seconds
+    for (let singleWait = 1; singleWait <= singleWaitMax; singleWait *= 2) {
+        const result = await code();
+        if (result !== TryAgain) {
+            Log.Logger.info(`Done waiting for ${description}`);
+            return result;
+        }
+        Log.Logger.info(`Still waiting for ${description}. Will retry in ${singleWait} seconds`);
+        await Util.sleep(singleWait*1000);
+    }
+    throw new Error(`Timed out waiting for ${description}`);
+}
+
 // Gets PR metadata from GitHub
 // If requested and needed, retries until GitHub calculates PR mergeable flag.
 // Those retries, if any, are limited to a few minutes.
 export async function getPR(prNum, awaitMergeable) {
-    const max = 64 * 1000 + 1; // ~2 min. overall
-    for (let d = 1000; d < max; d *= 2) {
+    return await waitFor('GitHub to calculate mergeable attribute', async () => {
         const pr = await getRawPR(prNum);
         // pr.mergeable is useless (and not calculated?) for a closed PR
         if (pr.mergeable !== null || pr.state === 'closed' || !awaitMergeable)
             return pr;
-        Log.Logger.info("PR" + prNum + ": GitHub still calculates mergeable attribute. Will retry in " + (d/1000) + " seconds");
-        await Util.sleep(d);
-    }
-    return Promise.reject(new ErrorContext("Timed out waiting for GitHub to calculate mergeable attribute",
-                getPR.name, {pr: prNum}));
+        return TryAgain;
+    });
 }
 
 // gets a PR from GitHub (as is)
@@ -254,26 +270,6 @@ export async function getComments(prNum) {
     return comments;
 }
 
-// XXX: remove if not needed, since the "required_status_checks" api call sometimes
-// does not work(?) for organization repositories (returns 404 Not Found).
-//async function getProtectedBranchRequiredStatusChecks(branch) {
-//    let params = commonParams();
-//    params.branch = branch;
-//    const promise = new Promise( (resolve, reject) => {
-//      GitHub.authenticate(GitHubAuthentication);
-//      GitHub.repos.getProtectedBranchRequiredStatusChecks(params, (err, res) => {
-//          if (err) {
-//             reject(new ErrorContext(err, getProtectedBranchRequiredStatusChecks.name, params));
-//             return;
-//          }
-//          const result = {checks: res.data.contexts.length};
-//          logApiResult(getProtectedBranchRequiredStatusChecks.name, params, result);
-//          resolve(res);
-//      });
-//    return (await rateLimitedPromise(promise)).contexts;
-//    });
-//}
-
 export async function createStatus(sha, state, targetUrl, description, context) {
     assert(!Config.dryRun());
 
@@ -286,7 +282,20 @@ export async function createStatus(sha, state, targetUrl, description, context) 
 
     const result = await GitHub.rest.repos.createCommitStatus(params);
     logApiResult(createStatus.name, params, {context: result.data.context});
-    return (await rateLimitedPromise(result)).context;
+    await rateLimitedPromise(result);
+
+    const checkParams = commonParams();
+    checkParams.ref = sha;
+    await waitFor(`GitHub to create ${context} status for the ${sha} commit`, async () => {
+        const checkResult = await GitHub.rest.repos.getCombinedStatusForRef(checkParams);
+        await rateLimitedPromise(checkResult);
+        const statuses = checkResult.data.statuses;
+        if (statuses.some(st => st.context === context))
+            return statuses;
+        return TryAgain;
+    });
+
+    return result.data.context;
 }
 
 export async function getProtectedBranchRequiredStatusChecks(branch) {

src/MergeContext.js
@@ -453,7 +453,7 @@ class Labels
         try {
             await GH.removeLabel(name, this._prNum);
         } catch (e) {
-            if (e.name === 'ErrorContext' && e.notFound()) {
+            if (e instanceof RequestError && e.status === 404) {
                 Log.LogException(e, "_removeFromGitHub: " + name + " not found");
                 return;
             }
@@ -1040,7 +1040,7 @@ class PullRequest {
         try {
             this._contextsRequiredByGitHubConfig = await GH.getProtectedBranchRequiredStatusChecks(this._prBaseBranch());
         } catch (e) {
-           if (e.name === 'ErrorContext' && e.notFound())
+           if (e instanceof RequestError && e.status === 404)
                this._logEx(e, "no status checks are required");
            else
                throw e;
@@ -1676,7 +1676,8 @@ class PullRequest {
         try {
             await GH.updateReference(this._prBaseBranchPath(), this._stagedSha(), false);
         } catch (e) {
-            if (e.name === 'ErrorContext' && e.unprocessable()) {
+            if (e instanceof RequestError && e.status === 422) {
+                // fast-forward failure returns 422 (unprocessable entity)
                 await this._stagedPosition.compute();
                 if (this._stagedPosition.diverged())
                     this._log("could not fast-forward, the base " + this._prBaseBranchPath() + " was probably modified while we were merging");

src/Util.js
@@ -31,44 +31,6 @@ export function ParsePrNumber(prMessage) {
     return prNumber;
 }
 
-// An error context for promisificated wrappers.
-export class ErrorContext extends Error {
-    // The underlying rejection may be a bot-specific Promise.reject() or
-    // be caused by a GitHub API error, so 'err' contains either
-    // an error string or the entire API error object.
-    constructor(err, method, args) {
-        assert(err);
-        let msg = "";
-        if (method !== undefined)
-            msg = method + ", ";
-        msg += "Error: " + JSON.stringify(err);
-        if (args !== undefined)
-            msg += ", params: " + JSON.stringify(args);
-        super(msg);
-        this.name = this.constructor.name;
-        Error.captureStackTrace(this, this.constructor);
-        this._err = err;
-    }
-
-    // 404 (Not found)
-    notFound() {
-        if (this._err.name === "HttpError")
-            return this._err.code === 404;
-        // We treat our local(non-API) promise rejections as
-        // if the requested resource was 'not found'.
-        // TODO: rework if this simple approach does not work.
-        return true;
-    }
-
-    // 422 (unprocessable entity).
-    // E.g., fast-forward failure returns this error.
-    unprocessable() {
-        if (this._err.name === "HttpError")
-            return this._err.code === 422;
-        return false;
-    }
-}
-
 // Identifies or refers to a PR using either
 // PR number, or
 // PR branch name (without 'refs' or 'heads' prefixes), or